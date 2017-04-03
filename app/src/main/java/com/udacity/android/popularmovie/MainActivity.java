45c8dadd28842dafa758a128bda8da3bf1a48ae8
Grzegorz Bielański <gbielanski@poczta.onet.pl>
2017-04-03 14:26:52 +0200

    Recycler View added

--- app/src/main/java/com/udacity/android/popularmovie/MainActivity.java
+++ app/src/main/java/com/udacity/android/popularmovie/MainActivity.java
@@ -2,15 +2,33 @@ package com.udacity.android.popularmovie;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private TextView mErrorMessageTextView;
+    private ProgressBar mProgresBar;
+    private MoviePosterAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRecyclerView = (RecyclerView)findViewById(R.id.rc_movie_grid);
+        mErrorMessageTextView = (TextView)findViewById(R.id.tv_error_message);
+        mProgresBar = (ProgressBar)findViewById(R.id.pb_loading_progress);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        // COMPLETED (41) Set the layoutManager on mRecyclerView
+        mRecyclerView.setLayoutManager(layoutManager);
+        mAdapter = new MoviePosterAdapter();
+        mRecyclerView.setAdapter(mAdapter);
         Log.v("Movie key", "Movie key " + getString(R.string.movie_db_key));
     }
 }
---
+++ app/src/main/java/com/udacity/android/popularmovie/MoviePosterAdapter.java
@@ -0,0 +1,43 @@
+package com.udacity.android.popularmovie;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Grzegorz on 01.04.2017.
+ */
+
+public class MoviePosterAdapter extends RecyclerView.Adapter<MoviePosterAdapter.MovieViewHolder> {
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context ctx = parent.getContext();
+        int posterGridItemId = R.layout.poster_grid_item;
+        LayoutInflater inflater = LayoutInflater.from(ctx);
+        View view = inflater.inflate(posterGridItemId, parent, false);
+        MovieViewHolder holder = new MovieViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView mPosterImageView;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            mPosterImageView  = (ImageView)itemView.findViewById(R.id.img_poster_item);
+        }
+    }
+}
