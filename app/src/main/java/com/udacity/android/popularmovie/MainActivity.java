0db8c6e85163a50fa2d6ae067027db9188bd2f96
gbielanski <gbielanski@poczta.onet.pl>
2017-10-25 17:15:35 +0200

    Collapsing app bar in main activity added.

--- app/src/main/java/com/udacity/android/popularmovie/MainActivity.java
+++ app/src/main/java/com/udacity/android/popularmovie/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -78,15 +79,17 @@ public class MainActivity extends AppCompatActivity implements MoviePosterAdapte
     @BindView(R.id.tv_error_message) TextView mErrorMessageTextView;
     @BindView(R.id.pb_loading_progress) ProgressBar mProgressBar;
     @BindView(R.id.rc_movie_grid) RecyclerView mRecyclerView;
-    @BindView(R.id.my_toolbar) Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        setSupportActionBar(myToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar supportActionBar = getSupportActionBar();
+        if(supportActionBar != null)
+            supportActionBar.setDisplayShowTitleEnabled(false);
         GridLayoutManager layoutManager = new GridLayoutManager(this, numberOfColumns());
         mRecyclerView.setLayoutManager(layoutManager);
         mAdapter = new MoviePosterAdapter(this);
